a6d8eae798df15827d2852f3cdb3f35b7cc4f7df
Amit joshi <amit.joshi@centillionsofttech.com>
2023-06-29 15:52:56 +0530

    send mail using api in node

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,53 @@
 import express from "express";
 import nodemailer from "nodemailer";
+import { Request, Response } from "express";
 const app = express();
 
-const data = [
-  {
-    email: "joshiamit7383@gmail.com", // username
-  },
-  {
-    email: "amitjoshi6180@gmail.com",
-  },
-];
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.googlemail.com", // Gmail Host
-  port: 465, // Port
-  secure: true, // this is true as port is 465
-  auth: {
-    user: "curific.care@gmail.com", // username
-    pass: "yjkmaddiomewqfzj", // password
-  },
+app.use(
+  express.json({
+    type: "*/json",
+    strict: false,
+  })
+);
+app.get("/sendmail", (req: Request, res: Response) => {
+  const data = req.body;
+  console.log("data", data);
+  if (data.email !== "") {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.googlemail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "curific.care@gmail.com",
+        pass: "yjkmaddiomewqfzj",
+      },
+    });
+    var mailOptions = {
+      from: "curific.care@gmail.com",
+      to: data.email,
+      subject: "Sending Email using Node.js",
+      text: "hii this is the testing mode for using node js!",
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        return res.status(500).json({
+          message: "email Id not found in server!",
+          data: error,
+        });
+      } else {
+        return res.status(200).json({
+          message: "mail send successfully.",
+          data: data,
+          emailData: info.response,
+        });
+      }
+    });
+  } else {
+    return res.status(400).json({
+      messga: "Email is required",
+    });
+  }
 });
-for (let i = 0; i < data.length; i++) {
-  var mailOptions = {
-    from: "curific.care@gmail.com",
-    to: data[i].email,
-    subject: "Sending Email using Node.js",
-    text: "hii this is the testing mode for using node js!",
-  };
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
-}
 
 app.listen(8000, () => {
   console.log("server create successfully");
